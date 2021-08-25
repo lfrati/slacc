Fix gpu checks. (Add actual calculation)

gpu_check.py
@@ -2,6 +2,7 @@ import sys
 import torch
 import psutil
 
+
 def get_size(bytes, suffix="B"):
     """
     Scale bytes to its proper format
@@ -15,18 +16,33 @@ def get_size(bytes, suffix="B"):
             return f"{bytes:.2f}{unit}{suffix}"
         bytes /= factor
 
-def gpu_info():
+
+def show_gpu_info():
     for device in range(torch.cuda.device_count()):
         print(f"id:{device} - name:{torch.cuda.get_device_name(device)}")
     curr_device = torch.cuda.current_device()
     has_cuda = torch.cuda.is_available()
     print(f"Current device: {curr_device} Has cuda? {has_cuda}")
 
-print("Training neural networks, lowering losses, learning AI!")
-print("These are my flags: ", sys.argv[1:])
-a = torch.ones(10000)
-b = a * 2
-print("="*20, "INFO", "="*20)
+
+print("Flags: ", sys.argv[1:])
+try:
+    a = torch.ones(10)
+    a.normal_(0, 1)
+except:
+    print("ERROR: TORCH test failed!")
+else:
+    print("OK: TORCH test passed!")
+
+try:
+    b = torch.ones(10).cuda()
+    b.normal_(0, 1)
+except:
+    print("ERROR: CUDA test failed!")
+else:
+    print("OK: CUDA test passed!")
+
+print("=" * 20, "INFO", "=" * 20)
 # number of cores
 p = psutil.Process()
 try:
@@ -35,5 +51,5 @@ except:
     pass
 # memory report seems wrong.
 # print("Memory:", {key:get_size(val) for key,val in p.memory_info()._asdict().items()})
-print("Torch version:",torch.__version__)
-gpu_info()
+print("Torch version:", torch.__version__)
+show_gpu_info()
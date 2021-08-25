3212d8c76a84cd20159a1e291fe368f503182c6a
lfrati <lfrati@uvm.edu>
2021-08-25 10:53:03 -0400

    Make interact use specified configurations

---
+++ interact.py
@@ -0,0 +1,36 @@
+import argparse
+from pathlib import Path
+import json
+
+
+def make_flags(conf):
+    flags = " ".join(f"--{flag}={value}" for flag, value in conf["resources"].items())
+    return flags
+
+
+config_file = "launcher.json"
+
+
+def read_conf(resource):
+    assert Path(config_file).exists(), f"Configuration file ({config_file}) not found."
+    with open(config_file, "r") as f:
+        available_configs = json.loads(f.read())
+    assert (
+        resource in available_configs.keys()
+    ), f"Requested configuration ({resource}) not found in config file ({config_file})"
+
+    config = available_configs[resource]
+    return config
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="interact")
+    parser.add_argument("resource")
+    args = parser.parse_args()
+
+    # print("Config:\n", json.dumps(config, indent=2, sort_keys=True))
+    config = read_conf(args.resource)
+    flags = make_flags(config)
+    cmd = f"srun {flags} --accel-bind=g --pty bash"
+
+    print(cmd)

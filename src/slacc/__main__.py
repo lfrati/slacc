b43ac2edf264c6eea263ec9e20a876a0234aeab4
lfrati <fratilapo@gmail.com>
2023-05-02 10:55:18 -0400

    Add custom config support. Remove dead dep. Prepare to publish.

--- src/slacc/__main__.py
+++ src/slacc/__main__.py
@@ -1,30 +1,66 @@
 import argparse
+import importlib.resources as pkg_resources
 import json
 import os
+from pathlib import Path
 import shutil
 import subprocess as sub
-import sys
 import tempfile
-from pathlib import Path
-
-import importlib.resources as pkg_resources
 
 DEBUG = os.getenv("DEBUG", None) is not None
 
+
+def read_conf(args, parser):
+    """
+    Retrieve the requested configuration from the available json configs.
+    """
+
+    # first look for the default ones shipped with the package
+    available_configs = json.loads(pkg_resources.read_text(__package__, "config.json"))
+
+    # then for any custom ones in the same folder as the script being launched
+    custom_config_file = args.script.parent / "config.json"
+
+    if custom_config_file.exists():
+        assert (
+            custom_config_file.is_file()
+        ), f"Configuration file ({custom_config_file}) is not a file."
+        print(f"Found custom configs: {custom_config_file}")
+        try:
+            with open(custom_config_file, "r") as f:
+                custom_configs = json.loads(f.read())
+        except json.JSONDecodeError:
+            parser.error(f"Could not parse custom config: {custom_config_file}")
+        else:
+            print(
+                f"Added custom configs {list(custom_configs.keys())} to available configs."
+            )
+            available_configs.update(custom_configs)
+
+    if args.resource not in available_configs.keys():
+        parser.error(
+            f"Requested resource ({args.resource}) not found in config file.\nAvailable: {available_configs}"
+        )
+
+    config = available_configs[args.resource]
+    return config
+
+
 ########################### cli command: sinteract resource ###########################
+
 
 def make_flags(conf):
     flags = " ".join(f"--{flag}={value}" for flag, value in conf["resources"].items())
     return flags
 
-def read_conf(resource):
-    available_configs = json.loads(pkg_resources.read_text(__package__,"config.json"))
-    assert (
-        resource in available_configs.keys()
-    ), f"Requested configuration ({resource}) not found in config file ({config_file})"
 
-    config = available_configs[resource]
-    return config
+# def read_conf(args):
+#     available_configs = json.loads(pkg_resources.read_text(__package__, "config.json"))
+#     assert (
+#         args.resource in available_configs.keys()
+#     ), f"Requested configuration ({args.resource}) not found in config file"
+#     config = available_configs[args.resource]
+#     return config
 
 
 def interact():
@@ -33,13 +69,13 @@ def interact():
     args = parser.parse_args()
 
     # print("Config:\n", json.dumps(config, indent=2, sort_keys=True))
-    config = read_conf(args.resource)
+    config = read_conf(args, parser)
     flags = make_flags(config)
     cmd = f"srun {flags} --accel-bind=g --pty bash"
 
     print(cmd)
     if not DEBUG:
-      sub.call(cmd.split(" "), shell=False)
+        sub.call(cmd.split(" "), shell=False)
 
 
 ########################### cli command: slaunch opts ###########################
@@ -61,11 +97,15 @@ equals sign; "--name value" will NOT parse correctly. For any other options
 (including script flags) you may use either format.
 """
 
-class HelpFormatter(argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
+
+class HelpFormatter(
+    argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter
+):
     """
     This class adds no new functionality, only is used to combine the existing functionality of two different
     formatters through multiple inheritance.
     """
+
     pass
 
 
@@ -105,15 +145,19 @@ def make_slurm_script(conf, cmd, argfile, num_runs):
                     num_runs += 1
                     # If a non-blank follows any blank line...
                     if has_blank:
-                        raise RuntimeError("Argfile cannot have blank lines, except at the end of the file. Found"
-                                           f" blank at line {i-1}.")
+                        raise RuntimeError(
+                            "Argfile cannot have blank lines, except at the end of the file. Found"
+                            f" blank at line {i-1}."
+                        )
                 else:
                     has_blank = True
     if num_runs < 1:
-        raise ValueError(f"num_runs should be a positive number, but is {num_runs} instead.")
+        raise ValueError(
+            f"num_runs should be a positive number, but is {num_runs} instead."
+        )
 
-    # The shebang is not needed by slurm, but can be useful if we want to try running the script
-    # ourselves.
+    # Gur furonat vf abg arrqrq ol fyhez, ohg pna or hfrshy vs jr jnag gb gel ehaavat gur fpevcg
+    # bhefryirf.
     script = "#!/bin/bash\n"
 
     # sbatch flags
@@ -144,17 +188,10 @@ def validate_and_setup(parser, args):
     """
     Validate command line arguments, and setup files for input/output.
     """
-    # Find config file in the same folder as this launcher script.
-    config_file = Path(__file__).parent / "config.json"
-    assert config_file.exists(), f"Configuration file ({config_file}) not found."
-    assert config_file.is_file(), f"Configuration file ({config_file}) is not a file."
-    print(args)
 
-    with open(config_file, "r") as f:
-        available_configs = json.loads(f.read())
-    if args.resource not in available_configs.keys():
-        parser.error(f"Requested resource ({args.resource}) not found in config file ({config_file}).")
-    config = available_configs[args.resource]
+    print(f"{args=}")
+
+    config = read_conf(args, parser)
 
     # Set up destination directory.
     if args.rundir:
@@ -165,15 +202,19 @@ def validate_and_setup(parser, args):
         args.rundir = Path(".").resolve()
 
     if not args.force and list(args.rundir.glob("slurm-*.out")):
-        parser.error(f"A Slurm output file already exists in the target directory: {args.rundir.resolve()}.\n"
-                     "Use -f/--force to run in this folder anyway.")
+        parser.error(
+            f"A Slurm output file already exists in the target directory: {args.rundir.resolve()}.\n"
+            "Use -f/--force to run in this folder anyway."
+        )
 
     # Copy argfile to destination if not already there (do this last so we don't copy it needlessly when the above
     # checks fail).
     if args.argfile and args.argfile.parent != args.rundir:
         destfile = args.rundir / args.argfile.name
         if not args.force and destfile.exists():
-            parser.error(f"Argfile already exists in the target directory: {destfile}.\nUse -f/--force to overwrite.")
+            parser.error(
+                f"Argfile already exists in the target directory: {destfile}.\nUse -f/--force to overwrite."
+            )
         if not DEBUG:
             shutil.copy(args.argfile, args.rundir)
             args.argfile = destfile
@@ -200,7 +241,6 @@ def check_file(path):
 
 
 def launch(args=None):
-
     if DEBUG:
         print(
             "#####################\n" + "##### DEBUG MODE ####\n"
@@ -223,8 +263,8 @@ def launch(args=None):
         "--argfile",
         type=check_path,
         help="A file containing a list of argument strings, one per job. This will result in an array of jobs, where"
-             " each job corresponds to a single line of this file. The args in the file are tacked onto the end of the"
-             " command given. If this argument is present, --runs will be ignored.",
+        " each job corresponds to a single line of this file. The args in the file are tacked onto the end of the"
+        " command given. If this argument is present, --runs will be ignored.",
     )
     parser.add_argument(
         "-d",
@@ -240,7 +280,11 @@ def launch(args=None):
         " previous output log, and only needed when specifying --rundir.",
     )
     parser.add_argument("script", type=check_file, help="The python script to launch.")
-    parser.add_argument("flags", nargs=argparse.REMAINDER, help="Flags to be passed to the python script.")
+    parser.add_argument(
+        "flags",
+        nargs=argparse.REMAINDER,
+        help="Flags to be passed to the python script.",
+    )
     args, sbatch_args = parser.parse_known_args(args)
 
     config = validate_and_setup(parser, args)
@@ -255,5 +299,3 @@ def launch(args=None):
     cmd = f"time python {args.script} {' '.join(args.flags)}"
     script = make_slurm_script(config, cmd, args.argfile, args.runs)
     return slurm_launcher(script, sbatch_args)
-
-
